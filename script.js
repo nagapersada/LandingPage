ac94fe81e2fcd8602944eab93c0122242a321a69
nagapersada <kharismanagapersada@gmail.com>
2026-01-08 10:54:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // DATABASE MEMBER
 const members = {
     'deka': { name: "Deka Sonjaya", photo: "foto-deka.jpg", wa: "6285524444037", rank: 6 },
-    'nur': { name: "Sudarsi Nur A..", photo: "foto-nur.jpg", wa: "6281331724817", rank: 1 },
+    'nur': { name: "Sudarsi Nur A.", photo: "foto-nur.jpg", wa: "6281331724817", rank: 1 },
     'yunus': { name: "Yunus H.", photo: "foto-yunus.jpg", wa: "62 81231688816", rank: 0 },
     'ntes': { name: "Barkah Tresna Daya", photo: "foto-ntes.jpg", wa: "6281320569317", rank: 0 },
     'ina': { name: "Inna Garnia", photo: "foto-ina.jpg", wa: "6282295683474", rank: 2 },
