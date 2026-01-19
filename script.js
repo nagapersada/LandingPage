955998525c4d38400a5a92e659c201dacef6fe8c
nagapersada <kharismanagapersada@gmail.com>
2026-01-19 17:07:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // DATABASE MEMBER
 const members = {
     'deka': { name: "Deka Sonjaya", photo: "foto-deka.jpg", wa: "6285524444037", rank: 6 },
+    'dada': { name: "Dada Juhada", photo: "foto-dada.jpg", wa: "6289527098680", rank: 0 },
     'rianto': { name: "Rianto Nugraha", photo: "foto-rianto.jpg", wa: "62 85864677996", rank: 0 },
     'widia': { name: "Widia Sutiani", photo: "foto-widia.jpg", wa: "6281233422232", rank: 0 },
     'alia': { name: "Alia Sabila", photo: "foto-alia.jpg", wa: "628989269556", rank: 0 },
